f8b696271ed72f08a63d02c04af1e64ebb6507a6
elvinos <aep.charles@gmail.com>
2017-10-02 13:44:45 +0100

    Alex Add

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Cinema!");
-        System.out.println("Hello Claudia");
+
+        ArrayList<String[]> userData = new ArrayList<>();
+
+        for (int n = 0; n < 2; n++) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Please enter your name: ");
+            String name = input.next();
+            System.out.print("Please enter your email: ");
+            String email = input.next();
+            System.out.print("Please enter your password: ");
+            String password = input.next();
+            userData.add(new String[]{name, email, password});
+            System.out.println(Arrays.toString(userData.get(n)));
+        }
+
     }
 }
