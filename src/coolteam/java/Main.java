a16ce83bb01bbbca688e318242d0def3eb12c9f7
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-10-11 15:26:03 +0100

    Separated read and write files

--- src/coolteam/java/Main.java
+++ src/coolteam/java/Main.java
@@ -1,49 +1,14 @@
 package coolteam.java;
 
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        ArrayList<String[]> userData = new ArrayList<>();
+        WriteUserData.writeUserData();
 
-        String addUser;
-
-        do {
-            Scanner input = new Scanner(System.in);
-
-            System.out.print("Please enter your name: ");
-            String name = input.next();
-
-            System.out.print("Please enter your email: ");
-            String email = input.next();
-
-            System.out.print("Please enter your password: ");
-            String password = input.next();
-
-            userData.add(new String[]{name, email, password});
-
-            System.out.println("Do you wish to add another user? (y/n)");
-            addUser = input.next();
-
-        } while (addUser.equals("y"));
-
-        FileWriter writer = new FileWriter("output.txt");
-
-        System.out.println("User information:");
-
-        for (String[] i : userData) {
-            System.out.println(Arrays.toString(i));
-            writer.write(Arrays.toString(i));
-        }
-
-        writer.close();
+        Read.readFile("output.txt");
 
     }
-}§
+}
---
+++ src/coolteam/java/WriteUserData.java
@@ -0,0 +1,48 @@
+package coolteam.java;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WriteUserData {
+
+    public static void writeUserData() throws IOException {
+
+        ArrayList<String[]> userData = new ArrayList<>();
+
+        String addUser;
+
+        do {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Please enter your name: ");
+            String name = input.next();
+
+            System.out.print("Please enter your email: ");
+            String email = input.next();
+
+            System.out.print("Please enter your password: ");
+            String password = input.next();
+
+            userData.add(new String[]{name, email, password});
+
+            System.out.println("Do you wish to add another user? (y/n)");
+            addUser = input.next();
+
+        } while (addUser.equals("y"));
+
+        final FileWriter writer = new FileWriter("output.txt");
+
+        System.out.println("User information:");
+
+        for (String[] i : userData) {
+            System.out.println(Arrays.toString(i));
+            writer.write(Arrays.toString(i));
+        }
+
+        writer.close();
+    }
+
+}
