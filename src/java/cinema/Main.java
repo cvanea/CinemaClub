382e8846108ff3aacce5fdb614f931bccd86a539
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-10-11 14:29:40 +0100

    Made package structure

--- src/java/cinema/Main.java
+++ src/java/cinema/Main.java
@@ -1,4 +1,8 @@
+package java.cinema;
+
 import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
