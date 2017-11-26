ae6a6712fdade17d36db4845b01b296882f291cd
elvinos <aep.charles@gmail.com>
2017-11-26 22:02:13 +0000

    Tweaks to Profile GUI

--- src/cinemaclub/gui/ProfileGui.java
+++ src/cinemaclub/gui/ProfileGui.java
@@ -9,10 +9,10 @@ public class ProfileGui extends AnchorCinema{
 
 //    private Cinema cinema = new Cinema();
 
-    @FXML Label nameBox;
-    @FXML Label emailBox;
-    @FXML Label passwordBox;
-    @FXML Label staffIDBox;
+//    @FXML Label nameBox;
+//    @FXML Label emailBox;
+//    @FXML Label passwordBox;
+//    @FXML Label staffIDBox;
 
 //    nameBox.setText(cinema.getCurrentUser());
 
