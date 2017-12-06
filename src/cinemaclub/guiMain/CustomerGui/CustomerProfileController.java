4853a5343afa88b6f8b31a8ec9970d80355b681a
elvinos <aep.charles@gmail.com>
2017-12-06 18:48:53 +0000

    Changes to Profile Page

--- src/cinemaclub/guiMain/CustomerGui/CustomerProfileController.java
+++ src/cinemaclub/guiMain/CustomerGui/CustomerProfileController.java
@@ -10,11 +10,15 @@ public class CustomerProfileController extends CustomerMainController {
     @FXML Label nameBox;
     @FXML Label emailBox;
     @FXML Label passwordBox;
+    @FXML Label firstNameBox;
+    @FXML Label lastNameBox;
 
     @FXML private void initialize() {
         nameBox.setText(cinema.getProfileDetails().getUsername());
         emailBox.setText(cinema.getCurrentUser().getEmail());
         passwordBox.setText(cinema.getCurrentUser().getPassword());
+        firstNameBox.setText(cinema.getProfileDetails().getFirstName());
+        lastNameBox.setText(cinema.getProfileDetails().getSurname());
     }
 
     public void setProfileText(ActionEvent event) {
