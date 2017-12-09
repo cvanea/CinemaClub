47697f882e5b78cf865a5dcd671470691bd8a25f
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-12-09 12:16:39 +0000

    Fixed export text bug

--- src/cinemaclub/cinema/FilmEdit.java
+++ src/cinemaclub/cinema/FilmEdit.java
@@ -80,7 +80,7 @@ class FilmEdit {
         try {
             FileWriter writer = new FileWriter("Showings.csv");
 
-            writer.write("Film, Date, Time, Screen, Number of Available Seats, Number of Booked Seats, Booked Seats, Username\n");
+            writer.write("Film, Date, Time, Screen, Number of Booked Seats, Number of Available Seats, Booked Seats, Username\n");
 
             Map<Screen, ArrayList<Showing>> showings = screenRepository.getShowings();
             ArrayList<Showing> allShowings = new ArrayList<>();
