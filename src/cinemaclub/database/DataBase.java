08a1342d2477d18c287c6e1734fa751d31f917c8
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-12-13 19:16:21 +0000

    added films to database

--- src/cinemaclub/database/DataBase.java
+++ src/cinemaclub/database/DataBase.java
@@ -67,12 +67,23 @@ public class DataBase implements Serializable {
             dataBase.userRepository.addStaffID("3", "noStaff");
             dataBase.userRepository.addStaffID("4", "noStaff");
             dataBase.userRepository.addStaffID("5", "noStaff");
+            dataBase.userRepository.addStaffID("6", "noStaff");
 
-            Film up = new Film("UP", "/UP.jpg", "A great film", "02:00");
-            Film walle = new Film("Walle", "/walle.jpg", "A another great film", "02:00");
+            Film up = new Film("UP", "/UP.jpg", "Seventy-eight year old Carl Fredricksen travels to Paradise Falls in his home equipped with balloons, inadvertently taking a young stowaway.", "02:00");
+            Film walle = new Film("WALL·E", "/walle.jpg", "In the distant future, a small waste-collecting robot inadvertently embarks on a space journey that will ultimately decide the fate of mankind.", "02:00");
+            Film findingNemo = new Film("Finding Nemo", "/FindingNemo.jpg", "After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.", "02:00");
+            Film toyStory = new Film("Toy Story", "/ToyStory.jpg", "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.", "02:00");
+            Film princessMononoke = new Film("Princess Mononoke", "/PrincessMononoke.jpg", "On a journey to find the cure for a Tatarigami's curse, Ashitaka finds himself in the middle of a war between the forest gods and Tatara, a mining colony. In this quest he also meets San, the Mononoke Hime.", "02:00");
+            Film spiritedAway = new Film("Spirited Away", "/SpiritedAway.jpg", "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.", "02:00");
+            Film howlsMovingCastle = new Film("Howl's Moving Castle", "/HowlsMovingCastle.jpg", "When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.", "02:00");
 
             dataBase.filmRepository.addFilm("UP", up);
-            dataBase.filmRepository.addFilm("Walle", walle);
+            dataBase.filmRepository.addFilm("WALL·E", walle);
+            dataBase.filmRepository.addFilm("Finding Nemo", findingNemo);
+            dataBase.filmRepository.addFilm("Toy Story", toyStory);
+            dataBase.filmRepository.addFilm("Princess Mononoke", princessMononoke);
+            dataBase.filmRepository.addFilm("Spirited Away", spiritedAway);
+            dataBase.filmRepository.addFilm("Howl's Moving Castle", howlsMovingCastle);
 
             Screen screen1 = new Screen(1, 5, 10);
             Screen screen2 = new Screen(2, 8, 10);
