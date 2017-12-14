4268046fe8b024426b4113d9fe2924248d33e1a5
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-12-14 22:44:31 +0000

    added docs for user, staff, and customer

--- src/cinemaclub/user/Staff.java
+++ src/cinemaclub/user/Staff.java
@@ -1,17 +1,36 @@
 package cinemaclub.user;
 
+/**
+ * Class for staff users of the cinema system.
+ * Inherits most getters and setters from User.
+ */
 public class Staff extends User {
 
     private String staffId;
 
+    /**
+     * Inherits UserCredentials from User for instantiation.
+     *
+     * @param userCredentials profile details such as username, first name, email, etc.
+     */
     public Staff(UserCredentials userCredentials) {
         super(userCredentials);
     }
 
+    /**
+     * Gets staff ID for this staff.
+     *
+     * @return staff ID
+     */
     public String getStaffId() {
         return staffId;
     }
 
+    /**
+     * Sets staff ID for this staff.
+     *
+     * @param staffId staff ID
+     */
     public void setStaffId(String staffId) {
         this.staffId = staffId;
     }
--- src/cinemaclub/user/User.java
+++ src/cinemaclub/user/User.java
@@ -3,7 +3,8 @@ package cinemaclub.user;
 import java.io.Serializable;
 
 /**
- *
+ * Abstract class for users of the cinema system.
+ * Serializable allows it to be saved as bytes to an external database file.
  */
 public abstract class User implements Serializable {
 
@@ -12,56 +13,119 @@ public abstract class User implements Serializable {
     private UserCredentials userCredentials;
 
     /**
-     * @param userCredentials
+     * Subclasses of User must have UserCredentials at instantiation.
+     *
+     * @param userCredentials profile details such as username, first name, email, etc.
      */
     User(UserCredentials userCredentials) {
         this.userCredentials = userCredentials;
     }
 
+    /**
+     * Gets the username from userCredentials.
+     *
+     * @return username
+     */
     public String getUsername() {
         return userCredentials.getUsername();
     }
 
+    /**
+     * Gets the email from userCredentials.
+     *
+     * @return email
+     */
     public String getEmail() {
         return userCredentials.getEmail();
     }
 
+    /**
+     * Gets the password from userCredentials.
+     *
+     * @return password
+     */
     public String getPassword() {
         return userCredentials.getPassword();
     }
 
+    /**
+     * Gets the first name from userCredentials.
+     *
+     * @return first name
+     */
     public String getFirstName() {
         return userCredentials.getFirstName();
     }
 
+    /**
+     * Gets the surname from userCredentials.
+     *
+     * @return surname
+     */
     public String getSurname() {
         return userCredentials.getSurname();
     }
 
-    public void setPassword(String password) {
-        userCredentials.setPassword(password);
-    }
-
+    /**
+     * Sets the username from userCredentials.
+     *
+     * @param userName username
+     */
     public void setUsername(String userName) {
         userCredentials.setUsername(userName);
     }
 
+    /**
+     * Sets the email from userCredentials.
+     *
+     * @param email email
+     */
     public void setEmail(String email) {
         userCredentials.setEmail(email);
     }
 
+    /**
+     * Sets the password from userCredentials.
+     *
+     * @param password password
+     */
+    public void setPassword(String password) {
+        userCredentials.setPassword(password);
+    }
+
+    /**
+     * Sets the first name from userCredentials.
+     *
+     * @param firstName first name
+     */
     public void setFirstName(String firstName) {
         userCredentials.setFirstName(firstName);
     }
 
+    /**
+     * Sets the surname from userCredentials.
+     *
+     * @param surname surname
+     */
     public void setSurname(String surname) {
         userCredentials.setSurname(surname);
     }
 
+    /**
+     * Gets all userCredentials.
+     *
+     * @return userCredentials
+     */
     public UserCredentials getUserCredentials() {
         return userCredentials;
     }
 
+    /**
+     * Overrides toString method to clearly show all user properties.
+     * This takes the toString method from the userCredentials on the user.
+     *
+     * @return string of all user properties
+     */
     @Override
     public String toString() {
         return userCredentials.toString();
